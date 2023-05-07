c8a1d2ac2615314d556291ae51b866b1bdc85c15
MoriyaEster <moriyaeo100@gmail.com>
2023-05-07 23:47:35 +0300

    arranged

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -11,20 +11,6 @@
 using namespace ariel;
 using namespace std;
 
-Fraction::Fraction(float num)
-{
-    if (num == 0.0)
-    {
-        numerator = 0;
-        denominator = 1;
-    }
-    int new_num = num * 1000;
-    Fraction new_frac = Fraction(new_num, 1000);
-    Fraction reduc_frac = reduceFraction(new_frac);
-    numerator = reduc_frac.getNumerator();
-    denominator = reduc_frac.getDenominator();
-}
-
 Fraction::Fraction(int _numerator, int _denominator)
 {
 
@@ -45,6 +31,20 @@ Fraction::Fraction(int _numerator, int _denominator)
     denominator = _denominator / gcd_value;
 }
 
+Fraction::Fraction(float num)
+{
+    if (num == 0.0)
+    {
+        numerator = 0;
+        denominator = 1;
+    }
+    int new_num = num * 1000;
+    Fraction new_frac = Fraction(new_num, 1000);
+    Fraction reduc_frac = reduceFraction(new_frac);
+    numerator = reduc_frac.getNumerator();
+    denominator = reduc_frac.getDenominator();
+}
+
 Fraction::Fraction(double num)
 {
     *this = Fraction(float(num));
@@ -56,22 +56,25 @@ Fraction::Fraction()
     denominator = 1;
 }
 
+// ************************************************************//
+
 Fraction Fraction::operator+(const Fraction &other) const
 {
     long long lcm = (denominator * other.denominator) / gcd(denominator, other.denominator);
     long long new_numerator = (numerator * (lcm / denominator)) + (other.numerator * (lcm / other.denominator));
     long long new_denominator = lcm;
 
-    // Check for integer overflow before creating the new fraction object
     if (new_numerator > INT_MAX || new_numerator < INT_MIN || new_denominator > INT_MAX || new_denominator < INT_MIN)
     {
         throw overflow_error("Overflow error: the result of the multiplication is out of range for an int.");
     }
+
     if (new_denominator < 0)
     {
         new_denominator *= -1;
         new_numerator *= -1;
     }
+
     return reduceFraction(Fraction((int)new_numerator, (int)new_denominator));
 }
 
@@ -79,6 +82,7 @@ Fraction Fraction::operator-(const Fraction &other) const
 {
     long long new_nume = ((long long)numerator * other.denominator) - ((long long)other.numerator * denominator);
     long long new_denom = (long long)denominator * other.denominator;
+
     if (new_nume > INT_MAX || new_nume < INT_MIN || new_denom > INT_MAX || new_denom < INT_MIN)
     {
         throw std::overflow_error("Overflow error: the result of the multiplication is out of range for an int.");
@@ -89,6 +93,7 @@ Fraction Fraction::operator-(const Fraction &other) const
         new_denom *= -1;
         new_nume *= -1;
     }
+
     return reduceFraction(Fraction((int)new_nume, (int)new_denom));
 }
 
@@ -98,7 +103,6 @@ Fraction Fraction::operator*(const Fraction &other) const
     long long new_nume = (long long)this->getNumerator() * (long long)other.getNumerator();
     long long new_denom = (long long)this->getDenominator() * (long long)other.getDenominator();
 
-    // Check for overflow
     if (new_nume > INT_MAX || new_nume < INT_MIN || new_denom > INT_MAX || new_denom < INT_MIN)
     {
         throw overflow_error("Overflow error: the result of the multiplication is out of range for an int.");
@@ -119,6 +123,7 @@ Fraction Fraction::operator/(const Fraction &other) const
     {
         throw runtime_error("can't devide by zero");
     }
+
     long long this_nume = this->getNumerator();
     long long this_denom = this->getDenominator();
     long long other_nume = other.getNumerator();
@@ -158,9 +163,9 @@ bool Fraction::operator!=(const Fraction &other) const
 
 bool Fraction::operator<(const Fraction &other) const
 {
-
     double this_value = (double)numerator / denominator;
     double other_value = (double)other.numerator / other.denominator;
+
     return this_value < other_value;
 }
 
@@ -203,6 +208,7 @@ Fraction Fraction::operator/(float other)
         throw std::runtime_error("can't devide by zero");
     }
     Fraction other_frac = Fraction(other);
+
     return this->operator/(other_frac);
 }
 
@@ -261,7 +267,9 @@ Fraction ariel::operator/(float num, const Fraction &frac)
     {
         throw std::runtime_error("can't devide by zero");
     }
+    
     Fraction other_frac = Fraction(num);
+
     return other_frac.operator/(frac);
 }
 
